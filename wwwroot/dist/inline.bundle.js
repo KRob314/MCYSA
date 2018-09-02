a6da706996b5cf49034f56b1940dd0dd9aa515e0
KRob314 <krob636@gmail.com>
2018-09-01 23:30:51 -0400

    update packages

--- wwwroot/dist/inline.bundle.js
+++ wwwroot/dist/inline.bundle.js
@@ -90,7 +90,7 @@
 /******/ 	
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "01040e638b54b0bbcfcb"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "4438be49356fbfd79393"; // eslint-disable-line no-unused-vars
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule; // eslint-disable-line no-unused-vars
